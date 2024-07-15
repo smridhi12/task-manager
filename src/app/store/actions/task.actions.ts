f5b581e73c092ebc37b892bfb9da33549da1d9ee
smridhi12 <smridhi1373.be21@chitkara.edu.in>
2024-07-15 18:52:11 +0530

    test

--- src/app/store/actions/task.actions.ts
+++ src/app/store/actions/task.actions.ts
@@ -1,17 +1,9 @@
 import { createAction, props  } from '@ngrx/store';
 import { Task } from '../../models/task.model';
 
-
-// Action to load tasks
 export const loadTasks = createAction('[Task] Load Tasks');
 export const loadTasksSuccess = createAction('[Task] Load Tasks Success', props<{ tasks: Task[] }>());
 export const loadTasksFailure = createAction('[Task] Load Tasks Failure', props<{ error: any }>());
-
-// Action to add a task
 export const addTask = createAction('[Task] Add Task', props<{ task: Task }>());
-
-// Action to update a task
 export const updateTask = createAction('[Task] Update Task', props<{ task: Task }>());
-
-// Action to delete a task
 export const deleteTask = createAction('[Task] Delete Task', props<{ id: number }>());
